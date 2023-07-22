bf6776f608b48b937b3b0a95b5b246b876e7ea7b
Bunje Kelly <kellybunje@gmail.com>
2023-07-22 08:33:55 -0700

    correction

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,27 +13,24 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, index;
 	int temp = 0;
-	int choice = 0;
 
-	if (size == 0 || size == 1)
-		return;
-	for (i = 0; i < size; i++)
+
+	for (i = 0; i < size - 1; i++)
 	{
 		index = i;
 
-		for (j = i; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[index])
 			{
 				index = j;
-				choice = 1;
 			}
 		}
-		if (choice == 1)
+		if (index != 1)
 		{
-			temp = array[index];
-			array[index] = array[i];
-			array[i] = temp;
+			temp = array[i];
+			array[i] = array[index];
+			array[index] = temp;
 			print_array(array, size);
 		}
 	}
